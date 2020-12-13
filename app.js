c5a6ad2b40eb30d5fcf8b0ea4e31091092e69588
Jessica Tax <69859383+MoveUpHealth@users.noreply.github.com>
2020-12-13 22:25:29 +0200

    created employee intake and write to file

--- app.js
+++ app.js
@@ -33,40 +33,108 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
-
-inquirer
-    .prompt([
-
-        { 
-            type: "input",
-            message: "Please enter the name of the team manager:",
-            name: "name",
-          },
-          {
-            type: "input",
-            message: "Manager's ID number:",
-            name: "id",
-          },
-          {
-            type: "input",
-            message: "Manager's emial:",
-            name: "email",
-          },
-          {
-            type: "input",
-            message: "Manager's office number:",
-            name: "officeNumber",
-          },
-    ]).then((answers) => {
-        const employees = []
-        const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber)
+const employees = []
+const collectManager = async () =>{
+const managerPrompts = [
+    { 
+        type: "input",
+        message: "Please enter the name of the team manager:",
+        name: "name",
+        },
+        {
+        type: "input",
+        message: "Manager's ID number:",
+        name: "id",
+        },
+        {
+        type: "input",
+        message: "Manager's emial:",
+        name: "email",
+        },
+        {
+        type: "input",
+        message: "Manager's office number:",
+        name: "officeNumber",
+        }
+    ]
+    
+    const managerAnswers = await inquirer.prompt(managerPrompts) 
+        
+        const manager = new Manager (managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber)
         employees.push(manager)
         console.log(employees)
-        const data = render(employees)
+        
+        
+
+    }
+
+    const collectEmployees = async (inputs = []) => {
+       
+        const prompts = [
+            { 
+                type: "list",
+                message: "Choose the employee's role:",
+                choices: ['Engineer', 'Intern'],
+                name: "role",
+              },
+            { 
+                type: "input",
+                message: "Please enter the name of the employee:",
+                name: "employeeName",
+              },
+              {
+                type: "input",
+                message: "Employees's ID number:",
+                name: "employeeId",
+              },
+              {
+                type: "input",
+                message: "Employee's emial:",
+                name: "employeeEmail",
+              },
+              {
+                type: "input",
+                message: "Employee's github account:",
+                name: "github",
+                when: (answers) => answers.role === 'Engineer'
+              },
+              {
+                type: "input",
+                message: "Intern's school:",
+                name: "school",
+                when: (answers) => answers.role === 'Intern'
+              },
+              {
+                type: 'confirm',
+                name: 'again',
+                message: 'Enter another employee? ',
+                default: true
+            }
+           
+            ]
+            
+            const {again, ...answers} = await inquirer.prompt(prompts)
+            const newInputs = [...inputs, answers]
+            return again ? collectEmployees(newInputs) : newInputs
+        }
+
+        const main = async () => {
+            const inputs = await collectEmployees ()
+            inputs.forEach( input => {
+                if(input.role === 'Engineer'){
+                    const engineer = new Engineer(input.employeeName, input.employeeId, input.employeeEmail, input.github)
+                    employees.push(engineer)
+                } else {
+                    const intern = new Intern(input.employeeName, input.employeeId, input.employeeEmail, input.school)
+                    employees.push(intern)
+                }})
+                console.log(inputs)
+                const data = render(employees)
         fs.writeFile(outputPath, data, "utf8", (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
          })
-        
-
-    })
+                
+            }
+            // collectManager()
+            main()
